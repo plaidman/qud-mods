228d3e22a68681d81f55fe66b0d4a0d2d019039b
jason tomsic <jrtomsic@gmail.com>
2024-08-08 19:29:39 -0400

    sort logic sorted

--- an-eye-for-value/Scripts/ZoneLootList/ZoneValueComparer.cs
+++ an-eye-for-value/Scripts/ZoneLootList/ZoneValueComparer.cs
@@ -12,9 +12,10 @@ namespace Plaidman.AnEyeForValue.Menus {
 
 			return xCat switch
 			{
-				1 => CompareDouble(x.Ratio ?? double.PositiveInfinity, y.Ratio ?? double.PositiveInfinity),
-				3 => CompareDouble(x.Value ?? double.PositiveInfinity, y.Value ?? double.PositiveInfinity),
-				2 or 4 => CompareDouble(y.Weight, x.Weight),
+				4 => CompareDouble(x.Ratio ?? double.PositiveInfinity, y.Ratio ?? double.PositiveInfinity),
+				5 => CompareDouble(x.Value ?? double.PositiveInfinity, y.Value ?? double.PositiveInfinity),
+				3 => CompareDouble(x.Weight, y.Weight),
+				2 => CompareDouble(y.Weight, x.Weight),
 				_ => 0,
 			};
 		}
@@ -26,24 +27,29 @@ namespace Plaidman.AnEyeForValue.Menus {
 		}
 
 		private int Category(InventoryItem item) {
-			// 1: known items sort by ratio, lowest first
-			// 2: unknown items sort by weight, highest first
-			// 3: zero weight sort by value, lowest first
-			// 4: unsellable (water container) sort by weight, highest first
+			// 5: zero weight sort by value, highest first
+			// 4: known items sort by ratio, highest first
+			// 3: unsellable (water container) sort by weight, highest first
+			// 2: unknown items sort by weight, lowest first
+			// 1: unknown liquids, all equal I guess
 
 			if (item.Ratio == null) {
-				return 4;
-			}
-
-			if (item.Weight <= 0) {
 				return 3;
 			}
 
+			if (item.Weight <= 0) {
+				return 5;
+			}
+
 			if (!item.IsKnown) {
+				if (item.IsPool) {
+					return 1;
+				}
+
 				return 2;
 			}
 
-			return 1;
+			return 4;
 		}
 	}
 }
