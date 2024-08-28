cc73b97e89355585079e36620d23fd761ac09a26
jason tomsic <jrtomsic@gmail.com>
2024-08-28 19:15:34 -0400

    simplify GetFactionMembers

--- salt-shuffle/Scripts/Utils.cs
+++ salt-shuffle/Scripts/Utils.cs
@@ -1,53 +1,34 @@
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.Remoting.Messaging;
 using XRL;
-using XRL.Rules;
 using XRL.World;
 using XRL.World.Parts;
 
 namespace Nalathni.SaltShuffle {
 	[HasGameBasedStaticCache]
 	class FactionUtils {
-		[GameBasedStaticCache]
-		public static List<Faction> FactionsWithMembers;
-		[GameBasedStaticCache]
-		public static List<GameObjectBlueprint> CreaturesWithFactions;
-		[GameBasedStaticCache]
-		public static Dictionary<string, List<GameObjectBlueprint>> FactionMemberCache;
+		private static List<Faction> FactionsWithMembers = null;
+		private static readonly Dictionary<string, List<GameObjectBlueprint>> FactionMemberCache = new();
 
 		[GameBasedCacheInit]
 		public static void InitModCache() {
-			CreaturesWithFactions = GameObjectFactory.Factory.BlueprintList.Where((bp) => {
-				return !bp.Tags.ContainsKey("BaseObject") && bp.HasPartParameter("Brain", "Factions");
-			}).ToList();
+			UnityEngine.Debug.Log("faction count in gamebasedcacheinit " + Factions.GetFactionCount());
 			
 			FactionsWithMembers = Factions.GetList().Where(f => {
 				return f.Visible && GameObjectFactory.Factory.AnyFactionMembers(f.Name);
 			}).ToList();
-			
-			FactionMemberCache = new();
 		}
 		
 		public static Faction GetRandomFaction() {
 			return FactionsWithMembers.GetRandomElementCosmetic();
 		}
 
-		// todo test if this caches between game loads
-		public static List<GameObjectBlueprint> GetFactionMembers(string faction) {
+		private static List<GameObjectBlueprint> GetFactionMembers(string faction) {
 			if (FactionMemberCache.TryGetValue(faction, out List<GameObjectBlueprint> factionMembers)) {
-				XRL.Messages.MessageQueue.AddPlayerMessage("DEBUG GFMIU cache hit " + faction);
 				return factionMembers;
 			}
 
-			XRL.Messages.MessageQueue.AddPlayerMessage("DEBUG GFMIU cache miss " + faction);
-
-			factionMembers = CreaturesWithFactions.Where((bp) => {
-				var factions = bp.GetPartParameter<string>("Brain", "Factions");
-				return factions.Contains(faction + "-100")
-					|| factions.Contains(faction + "-50")
-					|| factions.Contains(faction + "-25");
-			}).ToList();
+			factionMembers = GameObjectFactory.Factory.GetFactionMembers(faction);
 			FactionMemberCache.Add(faction, factionMembers);
 
 			return factionMembers;
