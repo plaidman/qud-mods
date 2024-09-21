f4194ab068f07f61b59ef8a97be8642e6bcec7a0
jason tomsic <jrtomsic@gmail.com>
2024-09-21 08:25:15 -0400

    card pouch uninstall works

--- salt-shuffle-revival/Scripts/CardPouch.cs
+++ salt-shuffle-revival/Scripts/CardPouch.cs
@@ -4,14 +4,23 @@ using System.Linq;
 
 namespace XRL.World.Parts {
 	[Serializable]
-	class SSR_CardPouch : IScribedPart {
+	class SSR_CardPouch : IScribedPart, IModEventHandler<SSR_UninstallEvent> {
 		public List<GameObject> Cards;
 
 		public List<SSR_Card> GetPartList() {
 			return Cards.Select(card => card.GetPart<SSR_Card>()).ToList();
 		}
 		
+		public override void Register(GameObject go, IEventRegistrar registrar) {
 		// todo drop cards on death
 		// todo handle uninstall
+			registrar.Register(The.Game, SSR_UninstallEvent.ID);
+			base.Register(go, registrar);
+		}
+
+		public bool HandleEvent(SSR_UninstallEvent e) {
+			ParentObject.RemovePart(this);
+			return base.HandleEvent(e);
+		}
 	}
 }
