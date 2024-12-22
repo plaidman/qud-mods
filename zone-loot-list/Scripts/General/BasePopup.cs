a8e9f173d15a993666b4b09e0bfa9be678b28d99
cyanea-bt <150618858+cyanea-bt@users.noreply.github.com>
2024-12-22 04:41:33 +0100

    override Popup's UIView with our custom one using Harmony This works pretty much flawlessly. The way we force our own UIView is obviously still a bit ugly but ZonePopup actually receives the keypresses from our custom keybind layer now. Works in modern UI, classic one still somewhat relies on "Auto" attribute of keybinds

--- zone-loot-list/Scripts/General/BasePopup.cs
+++ zone-loot-list/Scripts/General/BasePopup.cs
@@ -1,14 +1,76 @@
+using ConsoleLib.Console;
+using HarmonyLib;
 using System.Collections.Generic;
 using System.Linq;
+using XRL.UI;
 
 namespace Plaidman.AnEyeForValue.Menus {
 	public enum SortType { Weight, Value };
 	public enum PickupType { Single, Multi };
 
-	public class BasePopup {
+	// used for classic UI popups
+	// ref: XRL.UI.Popup
+	[UIView("ZoneLootPopup", false, false, false, "ZoneLootNav", null, false, 0, false)]
+	// used for modern UI popups
+	// ref: Qud.UI.PopupMessage
+	[UIView("DynamicZonePopupMessage", false, false, false, "ZoneLootNav", null, false, 0, false, IgnoreForceFullscreen = true)]
+	[UIView("PopupZoneMessage", false, false, false, "ZoneLootNav", "PopupMessage", false, 0, false, IgnoreForceFullscreen = true, UICanvasHost = 1)]
+	[HarmonyPatch]
+	public class BasePopup : IWantsTextConsoleInit {
 		public SortType CurrentSortType;
 		private Dictionary<SortType, List<InventoryItem>> ItemListCache;
 		protected Dictionary<SortType, IComparer<InventoryItem>> Comparers;
+
+		internal static bool callRealFunc = false;
+
+		[HarmonyPatch(typeof(GameManager), nameof(GameManager.PushGameView))]
+		[HarmonyPrefix]
+		static bool PushGV_Prefix(ref GameManager __instance, string NewView, bool bHard)
+        {
+			if (callRealFunc)
+            {
+				return true;
+            }
+			UnityEngine.Debug.LogError("PushGameView - Prefix - NewView: " + NewView);
+			if (NewView == "PopupMessage" && bHard)
+            {
+				callRealFunc = true;
+				GameManager.Instance.PushGameView("PopupZoneMessage", bHard);
+				return false;
+            }
+			else if (NewView == "DynamicPopupMessage" && bHard)
+            {
+				callRealFunc = true;
+				GameManager.Instance.PushGameView("DynamicZonePopupMessage", bHard);
+				return false;
+			}
+			else if (NewView == "Popup:Choice" && bHard)
+            {
+				callRealFunc = true;
+				GameManager.Instance.PushGameView("ZoneLootPopup", bHard);
+				return false;
+			}
+			return true;
+        }
+
+		[HarmonyPatch(typeof(GameManager), nameof(GameManager.PushGameView))]
+		[HarmonyPostfix]
+		static void PushGV_Postfix(ref GameManager __instance, string NewView, bool bHard)
+        {
+			if (callRealFunc)
+            {
+				callRealFunc = false;
+				GameManager.ViewInfo check = GameManager.Instance.GetViewData(NewView);
+				UnityEngine.Debug.LogError("PushGameView - NavCategory: " + check.NavCategory);
+			}
+        }
+
+		// copied from XRL.UI.Popup
+		public void Init(TextConsole TextConsole_, ScreenBuffer ScreenBuffer_)
+		{
+			Popup._TextConsole = TextConsole_;
+			Popup._ScreenBuffer = ScreenBuffer_;
+		}
 
 		protected void ResetCache() {
 			ItemListCache = new() {
