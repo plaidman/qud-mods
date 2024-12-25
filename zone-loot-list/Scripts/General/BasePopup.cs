4575903d63a1701febc90225d9c83ef19cc6c8fe
cyanea-bt <150618858+cyanea-bt@users.noreply.github.com>
2024-12-25 02:40:03 +0100

    add UpdateInput() HarmonyPatch for classic UI popups
    Allows us to remove the "Auto" attribute from all commands that use
    our own UIView. This attribute doesn't do anything for modern UI
    popups since those only care about the layer/navCategory attributes.
    For working with classic UI popups there's multiple options:
    
    Using Auto="DownPass" is not a good solution since the game won't
    warn the user when trying to bind 2 actions on the same layer to the
    same key.
    
    Using Auto="Down" should be fine in theory. At least as long as there
    aren't too many commands with Auto="Down" which use the same keybind
    (across different layers). Otherwise that could potentially lead to
    a situation where some commands are only processed sporadically since
    UpdateInput() returns as soon as it finds any active command among
    all entries with Auto="Down". I'm paraphrasing here but that is roughly
    how it works. Using Auto="Down" for commands on the "Adventure" layer
    is fine, since those have priority over other layers.
    
    Instead of using the Auto attribute we can simply add a Postfix to
    UpdateInput() and mimic what the game already does for its builtin
    layers. I.e. check if our own layer is currently active and only
    check/process our commands if it is.

--- zone-loot-list/Scripts/General/BasePopup.cs
+++ zone-loot-list/Scripts/General/BasePopup.cs
@@ -1,3 +1,4 @@
+using ConsoleLib.Console;
 using HarmonyLib;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,22 +8,20 @@ namespace Plaidman.AnEyeForValue.Menus {
 	public enum SortType { Weight, Value };
 	public enum PickupType { Single, Multi };
 
-	// used for classic UI popups
-	// ref: XRL.UI.Popup
+	// used for classic UI popups, ref: XRL.UI.Popup
 	[UIView("ZoneLootPopup", false, false, false, "ZoneLootNav", null, false, 0, false)]
-	// used for modern UI popups
-	// ref: Qud.UI.PopupMessage
+	// used for modern UI popups, ref: Qud.UI.PopupMessage
 	// using "PopupMessage" for the UICanvas parameter is intentional! Quote from the documentation (found on CoQ Discord):
 	// UICanvas - The name of the UI Canvas (Unity) GameObject which should be displayed when this view is active
-	[UIView("DynamicZonePopupMessage", false, false, false, "ZoneLootNav", null, false, 0, false, IgnoreForceFullscreen = true)]
-	[UIView("PopupZoneMessage", false, false, false, "ZoneLootNav", "PopupMessage", false, 0, false, IgnoreForceFullscreen = true, UICanvasHost = 1)]
+	[UIView("ZoneLootDynamicPopupMessage", false, false, false, "ZoneLootNav", null, false, 0, false, IgnoreForceFullscreen = true)]
+	[UIView("ZoneLootPopupMessage", false, false, false, "ZoneLootNav", "PopupMessage", false, 0, false, IgnoreForceFullscreen = true, UICanvasHost = 1)]
 	public class BasePopup {
 		public SortType CurrentSortType;
 		private Dictionary<SortType, List<InventoryItem>> ItemListCache;
 		protected Dictionary<SortType, IComparer<InventoryItem>> Comparers;
 
+		private static bool s_UIViewsLoaded = false;
 		internal static bool s_OverridePopup = false;
-		internal static bool s_UIViewsLoaded = false;
 
 		internal static bool Check_UIViewsLoaded()
         {
@@ -32,16 +31,16 @@ namespace Plaidman.AnEyeForValue.Menus {
 				return true;
 			}
 			bool nav_zlp = GameManager.Instance.HasViewData("ZoneLootPopup");
-			bool nav_dzpm = GameManager.Instance.HasViewData("DynamicZonePopupMessage");
-			bool nav_pzm = GameManager.Instance.HasViewData("PopupZoneMessage");
+			bool nav_dzpm = GameManager.Instance.HasViewData("ZoneLootDynamicPopupMessage");
+			bool nav_pzm = GameManager.Instance.HasViewData("ZoneLootPopupMessage");
 
 			// Try to register our custom UIViews if any of them aren't loaded yet
 			if (!nav_zlp || !nav_dzpm || !nav_pzm)
             {
 				GameManager.Instance.RegisterViews();
 				nav_zlp = GameManager.Instance.HasViewData("ZoneLootPopup");
-				nav_dzpm = GameManager.Instance.HasViewData("DynamicZonePopupMessage");
-				nav_pzm = GameManager.Instance.HasViewData("PopupZoneMessage");
+				nav_dzpm = GameManager.Instance.HasViewData("ZoneLootDynamicPopupMessage");
+				nav_pzm = GameManager.Instance.HasViewData("ZoneLootPopupMessage");
 
 				// If our custom UIViews still aren't loaded for some reason, return false
 				if (!nav_zlp || !nav_dzpm || !nav_pzm)
@@ -57,23 +56,23 @@ namespace Plaidman.AnEyeForValue.Menus {
 		[HarmonyPatch(typeof(GameManager), nameof(GameManager.PushGameView))]
 		private class PushGameViewPatch
         {
-			static bool Prefix(ref string NewView, bool bHard)
+			static bool Prefix(ref string NewView)
 			{
-				// Override NewView with our custom UIView once (only when called for by ZonePopup/InventoryPopup), otherwise use original
+				// Override NewView with our custom UIView (only when called for by ZonePopup/InventoryPopup), otherwise use original
 				if (s_OverridePopup)
 				{
 					s_OverridePopup = false;
-					// ref: XRL.UI.Popup.WaitNewPopupMessage(...)
-					if (NewView == "PopupMessage" && bHard)
+					// used for modern UI popups, ref: XRL.UI.Popup.WaitNewPopupMessage(...)
+					if (NewView == "PopupMessage")
 					{
-						NewView = "PopupZoneMessage";
+						NewView = "ZoneLootPopupMessage";
 					}
-					else if (NewView == "DynamicPopupMessage" && bHard)
+					else if (NewView == "DynamicPopupMessage")
 					{
-						NewView = "DynamicZonePopupMessage";
+						NewView = "ZoneLootDynamicPopupMessage";
 					}
-					// ref: XRL.UI.Popup.PickOption(...)
-					else if (NewView == "Popup:Choice" && bHard)
+					// used for classic UI popups, ref: XRL.UI.Popup.PickOption(...)
+					else if (NewView == "Popup:Choice")
 					{
 						NewView = "ZoneLootPopup";
 					}
@@ -85,6 +84,38 @@ namespace Plaidman.AnEyeForValue.Menus {
 				return true; // return control to the original function
 			}
 		}
+
+		// Required to make our UIView work with classic UI Popups
+		// This mimics what the game already does for its own layers/navCategories
+		[HarmonyPatch(typeof(GameManager), nameof(GameManager.UpdateInput))]
+		private class UpdateInputPatch
+        {
+			static void Postfix (GameManager __instance)
+            {
+				//UnityEngine.Debug.LogError("ZoneLootList - UpdateInputPatch1");
+				if (__instance.CurrentGameView.StartsWith("ZoneLoot"))
+                {
+					string navCat = __instance.GetViewData(__instance.CurrentGameView).NavCategory;
+					string layer = "ZoneLootLayer";
+					//UnityEngine.Debug.LogError("ZoneLootList - UpdateInputPatch2 - " + navCat);
+					//UnityEngine.Debug.LogError(string.Join(", ", CommandBindingManager.CommandBindingLayers));
+					if (navCat == "ZoneLootNav" && CommandBindingManager.CommandBindingLayers.ContainsKey(layer))
+                    {
+						var list = CommandBindingManager.CommandBindingLayers[layer];
+						//UnityEngine.Debug.LogError("ZoneLootList - UpdateInputPatch3");
+						foreach (var entry in list.actions)
+						{
+							if (ControlManager.isCommandDown(entry.name, true, false, false))
+							{
+								//UnityEngine.Debug.LogError("ZoneLootList - UpdateInputPatch4");
+								Keyboard.PushCommand(entry.name, null);
+								//return;
+							}
+						}
+					}
+				}
+            }
+        }
 
 		protected void ResetCache() {
 			ItemListCache = new() {
